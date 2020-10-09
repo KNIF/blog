147ef561cd44bed2dadc29919ae5ce9254384844
KNIF <admin@knif.dev>
2020-10-09 09:46:19 +0200

    Minified GTAG HTML

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,21 +5,13 @@ export default class MyDocument extends Document {
     return (
       <Html lang='en' className='bg-custom-bg text-custom-fg'>
         <Head>
-          {/* Global Site Tag (gtag.js) - Google Analytics */}
           <script
             async
             src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_TRACKING_ID}`}
           />
           <script
             dangerouslySetInnerHTML={{
-              __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
-          `,
+              __html: `window.dataLayer = window.dataLayer || []; function gtag() { dataLayer.push(arguments); } gtag('js', new Date()); gtag('config', '${process.env.GA_TRACKING_ID}', { page_path: window.location.pathname, });`,
             }}
           />
         </Head>
--- pages/index.tsx
+++ pages/index.tsx
@@ -20,6 +20,10 @@ const Index = ({ allPosts }: Props) => {
       <Layout>
         <Head>
           <title>KNIF</title>
+          <meta
+            property='og:image'
+            content='https://og-image.now.sh/**KNIF**.dev.jpeg?theme=dark&md=1&fontSize=100px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fvercel-triangle-white.svg'
+          />
         </Head>
         <Container>
           <Intro />
