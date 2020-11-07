2ef5b384c243d00404df09fd839f3ce5445c3635
KNIF <admin@knif.dev>
2020-11-07 13:20:40 +0100

    Fixed previous commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,22 +1,10 @@
 import { AppProps } from 'next/app';
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
-import * as gtag from '../lib/gtag';
 import '../styles/index.css';
 
-const MyApp = ({ Component, pageProps }: AppProps) => {
-  // const router = useRouter();
-  // useEffect(() => {
-  //   const handleRouteChange = (url: string) => {
-  //     gtag.pageview(url);
-  //   };
-  //   router.events.on('routeChangeComplete', handleRouteChange);
-  //   return () => {
-  //     router.events.off('routeChangeComplete', handleRouteChange);
-  //   };
-  // }, [router.events]);
-
-  return <Component {...pageProps} />;
-};
+const MyApp = ({ Component, pageProps }: AppProps) => (
+  <Component {...pageProps} />
+);
 
 export default MyApp;
