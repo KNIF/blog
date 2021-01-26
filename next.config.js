7ea7e67d382a5e2d37fe08ef130224a4b2b2edc9
KNIF <admin@knif.dev>
2021-01-26 14:39:21 +0100

    Proxied Analytics through Next.js Server

--- next.config.js
+++ next.config.js
@@ -5,12 +5,24 @@ module.exports = {
   async rewrites() {
     return [
       {
-        source: '/bee.js',
+        source: '/sb.js',
         destination: 'https://cdn.splitbee.io/sb.js',
       },
       {
-        source: '/_hive/:slug',
+        source: '/_sbh/:slug',
         destination: 'https://hive.splitbee.io/:slug',
+      },
+      {
+        source: '/um.js',
+        destination: 'https://umami.knif.dev/umami.js',
+      },
+      {
+        source: '/sa.js',
+        destination: 'https://sa.knif.dev/latest.js',
+      },
+      {
+        source: '/ns.gif',
+        destination: 'https://sa.knif.dev/noscript.gif',
       },
     ];
   },
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,22 +6,24 @@ export default class MyDocument extends Document {
       <Html lang='en' className='bg-custom-bg text-custom-fg'>
         <Head>
           {/* Splitbee Analytics */}
-          <script async data-api='/_hive' src='/bee.js'></script>
+          <script async data-api='/_sbh' src='/sb.js'></script>
+
           {/* umami Analytics */}
           <script
             async
             defer
             data-website-id='89d830f0-49ac-4dd0-a884-aa5dcae8a9ad'
-            src='https://umami.knif.dev/umami.js'
+            src='/um.js'
           ></script>
         </Head>
         <body>
           <Main />
           <NextScript />
+
           {/* Simple Analytics */}
-          <script async defer src='https://sa.knif.dev/latest.js'></script>
+          <script async defer src='/sa.js'></script>
           <noscript>
-            <img src='https://sa.knif.dev/noscript.gif' alt='' />
+            <img src='/ns.gif' alt='No JavaScript' />
           </noscript>
         </body>
       </Html>
